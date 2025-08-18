80280d943037a5af4992670dbaf6a89a6588232c
BELLOX <mantinoubello123@gmail.com>
2025-08-18 22:42:16 +0100

    Mise à jours de app du frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
