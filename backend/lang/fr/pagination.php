966fb018cbfd08a670cd3a1b887eb47d72a48de9
BELLOX <mantinoubello123@gmail.com>
2025-08-19 13:08:30 +0100

    Système de langue(fr/en)

---
+++ backend/lang/fr/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de langage pour la pagination
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes suivantes sont utilisées par la bibliothèque de pagination
+    | pour construire les liens de pagination simples. Vous pouvez les modifier
+    | selon vos besoins afin de mieux correspondre à votre application.
+    |
+    */
+
+    'previous' => '&laquo; Précédent',
+    'next' => 'Suivant &raquo;',
+
+];
---
+++ backend/lang/fr/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes de langage suivantes sont les messages par défaut qui
+    | correspondent aux raisons données par le courtier de mots de passe
+    | lors d’une tentative de mise à jour du mot de passe, comme un échec
+    | dû à un mot de passe ou un jeton de réinitialisation invalide.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Nous avons envoyé par e-mail le lien de réinitialisation de votre mot de passe.',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation du mot de passe est invalide.',
+    'user' => "Nous ne trouvons pas d’utilisateur avec cette adresse e-mail.",
+
+];
